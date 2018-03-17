ee601159f14969d1b953e6661dbd282338500671
Mehathab4eva <mehathab4eva@gmail.com>
2018-03-17 08:45:44 -0600

    second commit

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var keys = require("./key.js");
 var Twitter = require("twitter");
+ var Spotify = require('node-spotify-api');
 
 
 var arg1 = process.argv[2];
@@ -58,15 +59,125 @@ function myTweets() {
 					}; 
 	
 
+function spotify(){
+       
+        var songTitle;
+        if (arg2 === undefined || arg2=== " "){
+                   songTitle = "Ace of Base The Sign";
+               } else {
+                       songTitle = arg2; 
+                   }
+        for(var i = 3; i < process.argv.length; i++){
+          var space = " ";
+          songTitle = songTitle + space + process.argv[i];
+        }
+        
+        var spotify = new Spotify({
+	      id: keys.spotifyKeys.spotID,
+        secret: keys.spotifyKeys.spotSecret
+        });
+
+    spotify.search({ 
+      type: 'track', 
+      query: songTitle
+    }, 
+    function(err, data) {
+      if (err) {
+      return console.log('Error occurred: ' + err);
+      }
+ 
+  var object = data.tracks.items[0];
+  var artist = " Artist: " + object.album.artists[0].name;
+  var title = " Title: " + object.name;
+  var album = " Album: " + object.album.name;
+  var link = " Listen here: " + object.album.href;
+
+  var song = [artist, title, album, link]; 
+  
+      for (var i = 0; i < song.length; i++) {
+        console.log(song[i]);
+      };
+    }
+  )};
 
 
 
-switch(arg1) {
-	case 'my-tweets':
-			myTweets();
+function movie(){
+        var movieName;
+        if (arg2 === undefined || arg2=== " "){
+             movieName = "Mr Nobody";
+         } else {
+                 movieName = arg2;
+             }
 
-			break;
-	default:
-    		console.log("That's not enough! Add one of these commands: 'my-tweets', 'spotify-this-song', 'movie-this', or 'do-what-it-says.'"); 
+	var request = require("request");
+	request("http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=b3ed4a06", function(error, response, body) {
+
+  // If the request is successful
+if (!error && response.statusCode === 200) {
+
+	//Pull our data out of the JSON object.
+	title = "Title: " + JSON.parse(body).Title;
+	year = "Release Year: " + JSON.parse(body).Year;
+  rating = "IMDB Rating: " + JSON.parse(body).imdbRating;
+  rToms = "Tomatometer: " + JSON.parse(body).Ratings[0].Value;
+	lang = "Language: " + JSON.parse(body).Language;
+	plot = "Plot: " + JSON.parse(body).Plot;
+	actors = "Starring: " + JSON.parse(body).Actors;
+
+var film = [title, year, rating, rToms, lang, plot, actors];;
+
+    for (var i = 0; i < film.length; i++) {
+      console.log(film[i]);
+      }
+ 	  };
+ 	});
+}
+
+
+
+
+//the do-what-it-says command.
+function doWhat(){
+    fs.readFile("random.txt", "utf8", function(error, data) {
+      if (error) {
+        return console.log(error);
+      }
+      var dataArr = data.split(",");
+      
+      arg1 = dataArr[0];
+      arg2 = dataArr[1];
+     
+      
+      spotify(arg2);
+    }
+  )}; 
+
+switch (arg1) {
+  case 'do-what-it-says':
+    doWhat();
     break;
-    		}
+  case 'movie-this':
+    movie();
+    break
+  case 'spotify-this-song':
+    spotify();
+  break;
+  case 'my-tweets':
+    Tweets();
+  break;
+  default:
+    console.log("That's not enough! Add one of these commands: 'my-tweets', 'spotify-this-song', 'movie-this', or 'do-what-it-says.'"); 
+    break;
+}
+
+
+// switch(arg1) {
+// 	case 'my-tweets':
+// 			myTweets();
+
+// 			break;
+// 	default:
+//     		console.log("That's not enough! Add one of these commands: 'my-tweets', 'spotify-this-song', 'movie-this', or 'do-what-it-says.'"); 
+//     break;
+//     		}
--- key.js
+++ key.js
@@ -7,12 +7,12 @@ var twitterKeys = {
   access_token_secret: 'ite4tmC3ldD0SToLZ6hptVSUW2BYJPm95e9TcpalLbuH3'
 };
 
-// var	spotifyKeys = {
-//   consumer_key: '	89679zWtWvkhYi0rJnaNjBLxK',
-//   consumer_secret: 'c4du7Pe307wtBxo3tWVXqsk7Hqren1VvFsSSWzokOThhs6JbI4',
+var	spotifyKeys = {
+  spotID: '2bb8889bf01844cd83b4cc55e180f4f5',
+  spotSecret: '8221c60188864e37843297337987a542'
 //   access_token_key: '932896018039017472-RowQB8CwpggBDZiLnLj8NEUTq1ky49h',
 //   access_token_secret: 'ite4tmC3ldD0SToLZ6hptVSUW2BYJPm95e9TcpalLbuH3'
-// };
+ };
 
 // var imdbKeys = {
 //   consumer_key: '	89679zWtWvkhYi0rJnaNjBLxK',
@@ -22,7 +22,7 @@ var twitterKeys = {
 // };
 
 module.exports = {
-					twitter : twitterKeys
-					//spotify	: spotifyKeys,
+					twitter : twitterKeys,
+					spotify	: spotifyKeys
 					//imdb 	: imdbKeys
 				};
